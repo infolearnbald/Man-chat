a73d42df02647a1a6699cc5a4f1275d2355edbd3
InfoLearn <infolearn8441@gmail.com>
2025-12-07 23:19:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,32 @@
-self.addEventListener("install", e => {
-    self.skipWaiting();
-    e.waitUntil(
-        caches.open("manchat-v1").then(cache => {
-            return cache.addAll([
-                "index.html",
-                "register.html",
-                "chat.html",
-                "styles.css",
-                "app.js",
-                "manifest.json",
-                "icon-192.png",
-                "icon-512.png"
-            ]);
-        })
-    );
+const CACHE_NAME = 'man-chat-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/signup.html',
+  '/chat.html',
+  '/profile.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+// install
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+// activate
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
 });
 
+// fetch
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
