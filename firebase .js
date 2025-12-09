0f471a7062ce24d3d762d532b5b8cf6b67f5e705
InfoLearn <infolearn8441@gmail.com>
2025-12-09 22:59:02 +0200

    Atualizar o firebase .js

--- firebase .js
+++ firebase .js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCzsuBkwoJP_uxWEkIk2EWCOfqA560ij3M",
   authDomain: "darkchat-man.firebaseapp.com",
-  projectId: "darkchat-man",
+  projectId: "MAN-Chat",
   storageBucket: "darkchat-man.firebasestorage.app",
   messagingSenderId: "118052023438",
   appId: "1:118052023438:web:32de622f6d2d82041aeddf"
