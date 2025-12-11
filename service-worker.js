fe804ad183377727e942a77b46046e2636fe4e59
InfoLearn <infolearn8441@gmail.com>
2025-12-11 23:16:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,7 @@
 self.addEventListener("install", e => {
     self.skipWaiting();
-    e.waitUntil(
-        caches.open("manchat-v1").then(cache => {
-            return cache.addAll([
-                "index.html",
-                "register.html",
-                "chat.html",
-                "styles.css",
-                "app.js",
-                "manifest.json",
-                "icon-192.png",
-                "icon-512.png"
-            ]);
-        })
-    );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
 });
-
